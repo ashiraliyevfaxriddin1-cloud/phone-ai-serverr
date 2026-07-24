7b694fa6605b3dc660cbde309c4db6c308757a0a
ashiraliyevfaxriddin1-cloud <ashiraliyevfaxriddin1@gmail.com>
2026-07-24 12:49:36 +0500

    Set up Express server with chat endpoint

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require("express");
 
+const app = express();
+
+app.use(express.json());
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message;
+
+  res.json({
+    answer: "Men ishlayapman. Siz yozdingiz: " + message
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server ishladi");
+});
